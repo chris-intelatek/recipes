f81e166ff29e20bf71ee0f6e039da9930280a47a
Chris Cunningham <chris@intelatek.com>
2016-10-05 17:49:19 +0000

    add model for ratings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005155433) do
+ActiveRecord::Schema.define(version: 20161005171636) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "author"
+    t.string   "comment_entry"
+    t.integer  "rating"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
